f4d07a0a7b8dadf8aae2ce5727387a251ea6c39b
root <ray.dotropg@gmail.com>
2025-01-13 14:17:09 +0100

    0

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -3,20 +3,20 @@ import sys
 
 def factorial(n):
     if n < 0:
-    raise ValueError("Factorial is not defined for negative numbers.")
-    result = 1
+        raise ValueError("Factorial is not defined for negative numbers.")
+        result = 1
     while n > 1:
-    result *= n
-    n -= 1  # Diminue la valeur de n pour éviter une boucle infinie
+        result *= n
+        n -= 1  # Diminue la valeur de n pour éviter une boucle infinie
     return result
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
     try:
-    if len(sys.argv) < 2:
-    raise ValueError("Please provide a number as a command-line argument.")
+        if len(sys.argv) < 2:
+            raise ValueError("Please provide a number as a command-line argument.")
     num = int(sys.argv[1])
     f = factorial(num)
     print(f"Factorial of {num} is {f}")
-    except ValueError as e:
+except ValueError as e:
     print(f"Error: {e}")
 
